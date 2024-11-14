15fd8b3fc0de993757a5117cf698988dbd4049ed
iamvineetkumar <141312000+iamvineetkumar@users.noreply.github.com>
2024-11-14 17:57:01 +0530

    Update test-1.spec.ts

--- tests/e2e/api/test-1.spec.ts
+++ tests/e2e/api/test-1.spec.ts
@@ -2,12 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('one @sanity', async ({ page }) => {
   await page.goto('https://www.clari.com/');
-  await page.getByRole('link', { name: 'Login ' }).click();
-  await page.getByRole('navigation').getByRole('link', { name: 'Pricing' }).click();
-  await page.getByRole('link', { name: 'Solutions ' }).click();
-  await page.getByRole('link', { name: 'Products ' }).click();
-  await page.getByRole('link', { name: 'Why Clari' }).click();
-  // const page1Promise = page.waitForEvent('popup');
+  
   // await page.getByRole('banner').getByRole('link', { name: 'See Clari in action' }).click();
   // const page1 = await page1Promise;
   // await page.getByRole('link', { name: 'Clari', exact: true }).click();
@@ -16,27 +11,11 @@ test('one @sanity', async ({ page }) => {
 
 test('two @sanity', async ({ page }) => {
   await page.goto('https://www.clari.com/');
-  await page.getByRole('link', { name: 'Login ' }).click();
-  await page.getByRole('navigation').getByRole('link', { name: 'Pricing' }).click();
-  // await page.getByRole('link', { name: 'Solutions ' }).click();
-  // await page.getByRole('link', { name: 'Products ' }).click();
-  // await page.getByRole('link', { name: 'Why Clari' }).click();
-  // const page1Promise = page.waitForEvent('popup');
-  // await page.getByRole('banner').getByRole('link', { name: 'See Clari in action' }).click();
-  // const page1 = await page1Promise;
-  // await page.getByRole('link', { name: 'Clari', exact: true }).click();
+  
 });
 
 
 test('three @sanity', async ({ page }) => {
   await page.goto('https://www.clari.com/');
-  await page.getByRole('link', { name: 'Login ' }).click();
-  await page.getByRole('navigation').getByRole('link', { name: 'Pricing' }).click();
-  // await page.getByRole('link', { name: 'Solutions ' }).click();
-  // await page.getByRole('link', { name: 'Products ' }).click();
-  // await page.getByRole('link', { name: 'Why Clari' }).click();
-  // const page1Promise = page.waitForEvent('popup');
-  // await page.getByRole('banner').getByRole('link', { name: 'See Clari in action' }).click();
-  // const page1 = await page1Promise;
-  // await page.getByRole('link', { name: 'Clari', exact: true }).click();
+ 
 });
