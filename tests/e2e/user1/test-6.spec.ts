42db0a97a8247c15f346e18b95b13c44597a75d3
iamvineetkumar <141312000+iamvineetkumar@users.noreply.github.com>
2024-11-14 18:27:48 +0530

    Update test-6.spec.ts

--- tests/e2e/user1/test-6.spec.ts
+++ tests/e2e/user1/test-6.spec.ts
@@ -6,8 +6,14 @@ test('ten @sanity', async ({ page }) => {
 });
 
 
-test('eleven @smoke', async ({ page }) => {
-  await page.goto('https://www.msn.com/');
- 
+
+test('get started link @sanity', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started990099' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation345' })).toBeVisible();
 });
 
