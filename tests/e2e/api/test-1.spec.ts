4ce71ba4f91cd353922f19611b726e3e14e72caa
iamvineetkumar <141312000+iamvineetkumar@users.noreply.github.com>
2024-11-14 18:00:14 +0530

    Update test-1.spec.ts

--- tests/e2e/api/test-1.spec.ts
+++ tests/e2e/api/test-1.spec.ts
@@ -1,21 +1,19 @@
 import { test, expect } from '@playwright/test';
 
 test('one @sanity', async ({ page }) => {
-  await page.goto('https://www.clari.com/');
+  await page.goto('https://www.msn.com/');
   
-  // await page.getByRole('banner').getByRole('link', { name: 'See Clari in action' }).click();
-  // const page1 = await page1Promise;
-  // await page.getByRole('link', { name: 'Clari', exact: true }).click();
+ 
 });
 
 
 test('two @sanity', async ({ page }) => {
-  await page.goto('https://www.clari.com/');
+  await page.goto('https://www.msn.com/');
   
 });
 
 
 test('three @sanity', async ({ page }) => {
-  await page.goto('https://www.clari.com/');
+  await page.goto('https://www.msn.com/');
  
 });
