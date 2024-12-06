fc7fad2950ddd2b7ac4457e7bcb9cbb0131979cf
iamvineetkumar <141312000+iamvineetkumar@users.noreply.github.com>
2024-12-07 02:44:27 +0530

    Update test-7.spec.ts

--- tests/e2e/setup/test-7.spec.ts
+++ tests/e2e/setup/test-7.spec.ts
@@ -1,13 +1,13 @@
 import { test, expect } from '@playwright/test';
 
-test('six @sanity', async ({ page }) => {
+test('six @smoke', async ({ page }) => {
   await page.goto('https://www.msn.com/');
  
 });
 
 
 
-test('get started link @sanity', async ({ page }) => {
+test('get started link @smoke', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Click the get started link.
@@ -16,4 +16,17 @@ test('get started link @sanity', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation123312321' })).toBeVisible();
 });
+
+
+
+test('get started 1link @smoke', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started9888988888' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation123312321' })).toBeVisible();
+});
+
 
