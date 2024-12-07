8f13e74dd427960b179cb28678c4844f4a8b5cfb
iamvineetkumar <141312000+iamvineetkumar@users.noreply.github.com>
2024-12-08 01:15:19 +0530

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -21,8 +21,10 @@ module.exports = defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? "blob" : "html",
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+reporter: [
+    ['html', { outputFolder: 'playwright-report' }],
+    ['json', { outputFolder: 'playwright-report', outputFile: 'results.json' }]
+  ],  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
